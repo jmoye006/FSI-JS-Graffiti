785da569ba8a62a557bff1e99678970fe94c5196
Justin Moye <justin@Justins-MacBook-Pro.local>
2022-02-14 21:35:03 -0500

    nicechanges

--- index.js
+++ index.js
@@ -9,8 +9,4 @@ for(let i = 0; i < dogImages.length; i++){
     dogImages[i].style.borderRadius = '50px'
 }
 
-let dogNames = document.queryselectorAll('.dog-name')
-for(let i = 0; i < dogNames.length; i++){
-    dogNames[i].style.textAlign = 'left'
-}
 
