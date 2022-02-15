4fb36d9a78ba7f7ac646d7ffeaa24042a541d221
Justin Moye <justin@Justins-MacBook-Pro.local>
2022-02-14 21:29:34 -0500

    More changes

--- index.js
+++ index.js
@@ -4,7 +4,14 @@
 let header = document.querySelector('#page-header')
 header.style.textAlign = 'left'
 
-let dogImages = document.querySelectorAll('.dog-image')
+let dogImages = document.queryselectorAll('.dog-image')
 for(let i = 0; i < dogImages.length; i++){
     dogImages[i].style.borderRadius = '50px'
 }
+
+let dogNames = document.querySelector('.dog-name')
+dogNames.style.textAlign = 'left'
+
+let pageFooters = document.queryselector('.footer')
+pageFooters.style.color = 'orange'
+
