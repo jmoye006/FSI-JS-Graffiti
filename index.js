59fc7c779126f0b69232fe0025797550e97a5de2
Justin Moye <justin@Justins-MacBook-Pro.local>
2022-02-14 21:40:10 -0500

    4th changes

--- index.js
+++ index.js
@@ -4,9 +4,8 @@
 let header = document.querySelector('#page-header')
 header.style.textAlign = 'left'
 
-let dogImages = document.queryselectorAll('.dog-image')
+let dogImages = document.querySelectorAll('.dog-image')
 for(let i = 0; i < dogImages.length; i++){
     dogImages[i].style.borderRadius = '50px'
 }
-
 
